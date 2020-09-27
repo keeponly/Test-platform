7d1beb7e842720094be25619d8d307026edbb094
wangkai <1063699580@qq.com>
2020-09-27 19:45:41 +0800

    ajax

--- back-end/flask-basics/flask_01.py
+++ back-end/flask-basics/flask_01.py
@@ -3,16 +3,24 @@
 # @tel      :15313929271
 # @File     :flask_01
 import flask
-from flask import Flask
-
-app = Flask(__name__)
-
-
-# 添加路由
-@app.route('/')
-def index():
-    return 'hello'
-
-
-# 运行服务器
-app.run()
+from flask import Flask, request, render_template
+import os
+path = os.getcwd()
+print(path)
+# app = Flask(__name__,
+#             template_folder='D:\Test-platform\front-end\templates')
+#
+# # 添加路由
+# @app.route('/')
+# def index():
+#     a = request
+#     # 将请求参数存为字典
+#     args = request.args
+#     # 获取请求参数
+#     name =args.get('username')
+#     print(name)
+#     return render_template('home_page.html')
+#
+#
+# # 运行服务器
+# app.run()
