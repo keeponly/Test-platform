ae275da8038043f25fe4f5a49150f85b5d99e2a0
wangkai <1063699580@qq.com>
2020-09-27 18:39:16 +0800

    ajax

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,10 +1,18 @@
 $(function(){
     //ajax请求
     $('dl').click(function(){
-        });
+
     //获取账号
+    var user = $('#user').val();
     //获取密码
+    var pwd = $('#pwd').val();
     //相关加密处理
     //发送ajax请求
-
+    $.ajax({
+    url:'/login',
+    data:{'user':user,'pwd':pwd},
+    method:'post',
+    dataType:'json',
+    })
+    });
 })
