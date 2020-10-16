4f04ae1cc2ac743cdaa584925766f1c2e8380f9b
wangkai <1063699580@qq.com>
2020-10-16 17:17:32 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	back-end/flask-basics/flask_01.py
    #	front-end/templates/login_in.html

--- 线程与进程/队列/线程作业.py
+++ 线程与进程/队列/线程作业.py
@@ -6,5 +6,26 @@
 """
 1.用一个队列来存放商品
 2.创建一个生产商品线程类,当商品数量小于50,开始生产,每次200个商品,每生产完一轮暂停一秒
-3.创建一个消费商品线程类,当商品大于10时,开始消费,循环消费,每次消费3个,但商品数量小于10,暂停2秒
+3.创建五个消费商品线程类,当商品大于10时,开始消费,循环消费,每次消费3个,但商品数量小于10,暂停2秒
 """
+from queue import Queue
+from threading import Thread
+import time
+
+#创建一个队列,存储商品
+q = Queue()
+# 生产者线程
+class producer(Thread):
+    def run(self):
+        # 判断数量是否小于50，小于50就生产200个
+        count = 0
+        while True:
+            if q.qsize()<50:
+                count += 1
+                goods = '---第{}个商品---'.format(count)
+                q.put(goods)
+                print('生产', goods)
+            time.sleep(1)
+
+p = producer()
+p.start()
