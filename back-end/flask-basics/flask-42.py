001e6234551e40f32c6642b9639b163f36ff948a
1063699580@qq.com <1063699580@qq.com>
2020-10-23 09:33:05 +0800

    线程

--- back-end/flask-basics/flask-42.py
+++ back-end/flask-basics/flask-42.py
@@ -10,10 +10,16 @@ from flask import Flask, request, render_template, config
 app = Flask(__name__)
 
 
-@app.route('/case')
+@app.route('/case', methods=['POST'])
 def index():
-    id= request.args.get('id')  # 地址问号后面的
-    return f'{id}' # f相当于format
+    # 地址问号后面的,<int:id> http://127.0.0.1:5000/case?id=77
+    id = request.args.get('id')
+    return f'{id}'  # f相当于format
+
+
+"""/case与/case/两个不同的url   case/输入case可以访问   case/访问case不能访问
+308重定向
+"""
 
 
 if __name__ == '__main__':
---
+++ back-end/flask-basics/flask-42.集中制.py
@@ -0,0 +1,18 @@
+# @Time     :2020/10/22  22:11
+# @Author   :wang-kai
+# @tel      :15313929271
+# @File    :flask-42.集中制.py
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+def home():
+    return '123'
+
+
+app.add_url_rule('/', view_func=home)
+
+if __name__ == '__main__':
+    app.run(debug=True)
